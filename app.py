2b12f5e39c25b9db7c51f53599ab1ae80279fed9
Aditya bhardwaj <67585851+AdityaBhardwajDataScience@users.noreply.github.com>
2023-11-13 20:28:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    return 'index.html'
+    # return render_template('index.html')
 
 @app.route('/solve',methods='POST')
 def solve():
