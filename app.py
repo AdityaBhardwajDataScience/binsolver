2089cc192060c02ca899e51a6275a3a02484d3ac
Aditya bhardwaj <67585851+AdityaBhardwajDataScience@users.noreply.github.com>
2023-11-13 20:15:05 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template,request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/solve',methods='POST')
+def solve():
+    equation = request.form['eq']
+    return 'hello'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
